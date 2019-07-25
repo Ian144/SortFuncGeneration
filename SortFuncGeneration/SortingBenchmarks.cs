69f76a4c4b5c5c4045c1a98085f29633f51d53ea
Ian144 <ian144@hotmail.com>
2019-07-25 08:43:16 +0100

    consistent looking  benchmark times, sorting with Ordinal (not ignore case)

--- SortFuncGeneration/SortingBenchmarks.cs
+++ SortFuncGeneration/SortingBenchmarks.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Engines;
+using Nito.Comparers;
 using static System.String;
 
 namespace SortFuncGeneration
@@ -18,6 +19,7 @@ namespace SortFuncGeneration
         private readonly Consumer _consumer = new Consumer();
         private MyComparer<Target> _genTernComparer;
         private IOrderedEnumerable<Target> _lazyLinqOrderByThenBy;
+        private IComparer<Target> _nitoComparer;
 
 
         [IterationSetup]
@@ -35,16 +37,16 @@ namespace SortFuncGeneration
             {
                 new SortBy{PropName = "IntProp1", Ascending = true},
                 new SortBy{PropName = "StrProp1", Ascending = true},
-                new SortBy{PropName = "IntProp2", Ascending = false},
-                new SortBy{PropName = "StrProp2", Ascending = false},
+                new SortBy{PropName = "IntProp2", Ascending = true},
+                new SortBy{PropName = "StrProp2", Ascending = true},
             };
 
             // lazy, evaluated in a benchmark and in the check-valid function
             _lazyLinqOrderByThenBy = _xs
                 .OrderBy(x => x.IntProp1)
-                .ThenBy(x => x.StrProp1, StringComparer.OrdinalIgnoreCase)
-                .ThenByDescending(x => x.IntProp2)
-                .ThenByDescending(x => x.StrProp2, StringComparer.OrdinalIgnoreCase);
+                .ThenBy(x => x.StrProp1, StringComparer.Ordinal)
+                .ThenBy(x => x.IntProp2)
+                .ThenBy(x => x.StrProp2, StringComparer.Ordinal);
 
             Func<Target, Target, int> sortFunc = SortFuncCompiler.MakeSortFuncCompToMeth<Target>(sortBys);
 
@@ -54,6 +56,14 @@ namespace SortFuncGeneration
 
             Func<Target, Target, int> genTernSortFunc = SortFuncCompilerTernary.MakeSortFunc<Target>(sortBys);
             _genTernComparer = new MyComparer<Target>(genTernSortFunc);
+
+
+            _nitoComparer = ComparerBuilder.For<Target>()
+                .OrderBy(p => p.IntProp1)
+                .ThenBy(p => p.StrProp1, StringComparer.Ordinal)
+                .ThenBy(p => p.IntProp2)
+                .ThenBy(p => p.StrProp2, StringComparer.Ordinal);
+            //list.Sort(nameComparer);
         }
 
         private static int HandCoded(Target aa, Target bb)
@@ -63,13 +73,13 @@ namespace SortFuncGeneration
             int s1 = aa.IntProp1.CompareTo(bb.IntProp1);
             if (s1 != 0) return s1;
 
-            int s2 = CompareOrdinal(aa.StrProp1, bb.StrProp1);
+            int s2 = string.CompareOrdinal(aa.StrProp1, bb.StrProp1);
             if (s2 != 0) return s2;
 
-            int s3 = bb.IntProp2.CompareTo(aa.IntProp2);
+            int s3 = aa.IntProp2.CompareTo(bb.IntProp2);
             if (s3 != 0) return s3;
 
-            return CompareOrdinal(bb.StrProp2, aa.StrProp2);
+            return CompareOrdinal(aa.StrProp2, bb.StrProp2);
         }
 
         //private static int HandCoded(Target xx, Target yy)
@@ -80,11 +90,11 @@ namespace SortFuncGeneration
         //    int Sorter(Target aa, Target bb) =>
         //        tmp = aa.IntProp1.CompareTo(bb.IntProp1) != 0
         //            ? tmp
-        //            : tmp = bb.IntProp2.CompareTo(aa.IntProp2) != 0
+        //            : tmp = aa.IntProp2.CompareTo(bb.IntProp2) != 0
         //                ? tmp
-        //                : tmp = string.CompareOrdinal(aa.StrProp1, bb.StrProp1) != 0
+        //                : tmp = string.Compare(aa.StrProp1, bb.StrProp1, StringComparison.OrdinalIgnoreCase) != 0
         //                    ? tmp
-        //                    : string.CompareOrdinal(bb.StrProp2, aa.StrProp2);
+        //                    : string.Compare(aa.StrProp2, bb.StrProp2, StringComparison.OrdinalIgnoreCase);
 
         //    return Sorter(xx, yy);
         //}
@@ -99,14 +109,30 @@ namespace SortFuncGeneration
             var genSorted = _xs.OrderBy(tt => tt, _generatedComparer).ToList();
             var hcSorted = _xs.OrderBy(tt => tt, _handCodedComparer).ToList();
             var genTernComparerSorted = _xs.OrderBy(m => m, _genTernComparer).ToList();
-
+            var nitoSorted = _xs.OrderBy(m => m, _nitoComparer);
 
             bool hcOk = referenceOrdering.SequenceEqual(hcSorted);
             bool genSortedOk = referenceOrdering.SequenceEqual(genSorted);
             bool genTernaryOk = referenceOrdering.SequenceEqual(genTernComparerSorted);
+            bool nitoOk = referenceOrdering.SequenceEqual(nitoSorted);
 
-            //return hcOk && genSortedOk && genTernaryOk;
+
+            //for (int ctr = 0; ctr < referenceOrdering.Count; ++ctr)
+            //{
+            //    var refTarget = referenceOrdering[ctr];
+            //    var xxTarget = hcSorted[ctr];
+            //    if(refTarget != xxTarget)
+            //        Console.WriteLine($"failure at: {ctr}");
+            //}
+
+            //return hcOk && genSortedOk && genTernaryOk && nitoOk;
             return true;
+        }
+
+        [Benchmark]
+        public void NitoListSort()
+        {
+            _xs.Sort(_nitoComparer);
         }
 
         [Benchmark]
@@ -134,7 +160,7 @@ namespace SortFuncGeneration
         }
 
         [Benchmark]
-        public void OrderByThenByDesc()
+        public void LinqOrderByThenBy()
         {
             _lazyLinqOrderByThenBy.Consume(_consumer);
         }
